3ef5da60f75a01a198c711c7f9c4d11492be5c20
daltonfranco <daltonpacolafranco.git@hotmail.com>
2021-09-30 15:54:23 -0300

    herança, semana 10 da aula de java

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Funcionario f1 = new Funcionario();
+
+        f1.setNome("Dalton");
+
+        System.out.println(f1.getNome());
+    }
+}
